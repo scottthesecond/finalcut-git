0ebc38ee43bbcd43556c4e72feb468c3cab781c8
Scott <scotthoek@me.com>
2025-02-15 14:38:10 -0500

    Enhance error handling in logs.sh; add option to copy log file to desktop on error

--- user/functions/logs.sh
+++ user/functions/logs.sh
@@ -7,6 +7,6 @@ log_message() {
 handle_error() {
     echo "$1"
     log_message "ERROR: $1"
-    osascript -e "display dialog \"Error: $1.  See log for details.\" buttons {\"OK\"} default button \"OK\""
+    osascript -e "display dialog \"Error: $1. See log for details.\" buttons {\"Copy Log to Desktop\", \"OK\"} default button \"OK\"" -e "if button returned of result is \"Copy Log to Desktop\" then do shell script \"cp '$LOG_FILE' ~/Desktop/\""
     exit 1
 }
