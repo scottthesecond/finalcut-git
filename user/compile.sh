447d30ad33dfafdf370dbbb148d4410101430c65
Scott <scotthoek@me.com>
2025-06-27 11:55:04 -0400

    upped alpha version number; reverted config file loading to grep instead of source because of weirdness with source

--- user/compile.sh
+++ user/compile.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 PARENT_DIR=$(dirname "$SCRIPT_DIR")
 FUNCTIONS="$SCRIPT_DIR/functions"
-VERSION="3.0.0a3"
+VERSION="3.0.0a4"
 NAME="UNFlab"
 
 # Default values
--- user/functions/shared/config.sh
+++ user/functions/shared/config.sh
@@ -1,6 +1,7 @@
 # Load the .config file (for server and port if needed)
 if [ -f "$CONFIG_FILE" ]; then
-    source "$CONFIG_FILE"
+   # source "$CONFIG_FILE"
+   export $(grep -v '^#' "$CONFIG_FILE" | xargs)
     log_message "SERVER_PATH after config load: '$SERVER_PATH'"
 else
     setup
